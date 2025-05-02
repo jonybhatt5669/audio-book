f366369f1513dd2a4eb996cd18ddd5d2e8342cbe
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-05-03 03:52:46 +0600

    Bug: Title and author spacing fix

--- components/BookListItem.tsx
+++ components/BookListItem.tsx
@@ -3,18 +3,14 @@ import { AntDesign } from "@expo/vector-icons";
 import { Image, Text, View } from "react-native";
 export default function BookListItem({ book }: IBook) {
   return (
-    <View className="flex-row items-center  justify-between">
-      <View className="flex-row gap-2 items-center">
-        <Image
-          source={{ uri: book.thumbnail_url }}
-          className="aspect-square w-16"
-        />
-        <View className="gap-1">
-          <Text className="font-bold text-gray-100  text-2xl">
-            {book.title}
-          </Text>
-          <Text className="font-normal text-gray-400">{book.author}</Text>
-        </View>
+    <View className="flex-row items-center gap-2">
+      <Image
+        source={{ uri: book.thumbnail_url }}
+        className="aspect-square w-16"
+      />
+      <View className="gap-1 flex-1">
+        <Text className="font-bold text-gray-100  text-2xl">{book.title}</Text>
+        <Text className="font-normal text-gray-400">{book.author}</Text>
       </View>
       <View className="flex-row items-center gap-4">
         <AntDesign name="playcircleo" size={25} color="gainsboro" />
