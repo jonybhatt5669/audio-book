204b3b7060bd3feeb9a8f3caa5ad6b605d6033e3
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-05-03 03:16:25 +0600

    Added BookListItem Component

---
+++ components/BookListItem.tsx
@@ -0,0 +1,26 @@
+import { IBook } from "@/utils/interfaces/IBook";
+import { AntDesign } from "@expo/vector-icons";
+import { Image, Text, View } from "react-native";
+export default function BookListItem({ book }: IBook) {
+  return (
+    <View className="flex-row items-center  justify-between">
+      <View className="flex-row gap-2 items-center">
+        <Image
+          source={{ uri: book.thumbnail_url }}
+          className="aspect-square w-16"
+        />
+        <View className="gap-1">
+          <Text className="font-bold text-gray-100  text-2xl">
+            {book.title}
+          </Text>
+          <Text className="font-normal text-gray-400">{book.author}</Text>
+        </View>
+      </View>
+      <View className="flex-row items-center gap-4">
+        <AntDesign name="playcircleo" size={25} color="gainsboro" />
+        <AntDesign name="download" size={25} color="gainsboro" />
+        <AntDesign name="pluscircle" size={25} color="gainsboro" />
+      </View>
+    </View>
+  );
+}
