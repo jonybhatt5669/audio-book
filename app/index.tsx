f63b29cb6586bf8788cc24b1be5cda684b112fea
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-05-03 03:52:25 +0600

    Added flatlist

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
 import { BookListItem } from "@/components";
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
 import books from "../utils/dummyBooks";
 export default function Index() {
-  const book = books[0];
   return (
-    <>
-      <View className="bg-slate-800 flex-1 justify-center p-4">
-        <StatusBar style="auto" />
-        <BookListItem book={book} />
-      </View>
-    </>
+    <View className="bg-slate-800 flex-1 justify-center p-4 pt-10">
+      <FlatList
+        data={books}
+        contentContainerClassName="gap-6"
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <BookListItem book={item} />}
+      />
+      <StatusBar style="auto" />
+    </View>
   );
 }
