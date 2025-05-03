9718c13e9f47bcd03e160dc313ecaf5c0c7bf5cd
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-05-03 14:24:58 +0600

    Tab layout

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,29 @@
+import Foundation from "@expo/vector-icons/Foundation";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Tabs } from "expo-router";
+export default function MainLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Foundation name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="library"
+        options={{
+          title: "Library",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="library-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
