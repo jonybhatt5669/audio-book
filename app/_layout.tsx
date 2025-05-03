7ce507a88b3fce35281e880c5f126ad6d8a312f7
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-05-03 14:25:45 +0600

    Added theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,23 @@
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import "../global.css";
 
 export default function RootLayout() {
+  const theme = {
+    ...DarkTheme,
+    colors: {
+      ...DarkTheme.colors,
+    },
+  };
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    />
+    <ThemeProvider value={theme}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </ThemeProvider>
   );
 }
