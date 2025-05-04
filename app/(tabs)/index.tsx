d3a17978f963f18b5427d1c997e13ff9ef278fc5
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-05-04 18:34:45 +0600

    Fix not rendering issue

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
-import { BookListItem } from "@/components";
+import { AntDesign } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
-import { FlatList, View } from "react-native";
+import { FlatList, Image, Text, View } from "react-native";
 import books from "../../utils/dummyBooks";
 export default function Home() {
   return (
@@ -9,7 +9,25 @@ export default function Home() {
         data={books}
         contentContainerClassName="gap-6"
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <BookListItem book={item} />}
+        renderItem={({ item }) => (
+          <View className="flex-row items-center gap-2">
+            <Image
+              source={{ uri: item.thumbnail_url }}
+              className="aspect-square w-16"
+            />
+            <View className="gap-1 flex-1">
+              <Text className="font-bold text-gray-100  text-2xl">
+                {item.title}
+              </Text>
+              <Text className="font-normal text-gray-400">{item.author}</Text>
+            </View>
+            <View className="flex-row items-center gap-4">
+              <AntDesign name="playcircleo" size={22} color="gainsboro" />
+              <AntDesign name="download" size={22} color="gainsboro" />
+              <AntDesign name="pluscircle" size={22} color="gainsboro" />
+            </View>
+          </View>
+        )}
       />
       <StatusBar style="light" />
     </View>
