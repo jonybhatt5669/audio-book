98c3a19e5df4a8545b6640eca3a2608974155628
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-05-07 13:27:49 +0600

    Modified interface

--- utils/interfaces/IBook.ts
+++ utils/interfaces/IBook.ts
@@ -1,9 +1,7 @@
 export interface IBook {
-  book: {
-    id: string;
-    title: string;
-    author: string;
-    audio_url: string;
-    thumbnail_url: string;
-  };
+  id: string;
+  title: string;
+  author: string;
+  audio_url: string;
+  thumbnail_url: string;
 }
