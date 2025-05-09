d306db0efca79cd9a3cdcfdbe371e09065e5ed40
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-05-09 14:28:19 +0600

    updat books list

--- utils/dummyBooks.ts
+++ utils/dummyBooks.ts
@@ -1,90 +1,109 @@
 export default [
   {
     id: "1",
-    title: "1,000 A Plate",
-    author: " Jack McKenty",
+    title: "Breaking the Outer Ring: Marine Landings in the Marshall Islands",
+    author: "John C. Chapin",
     audio_url:
-      "https://ia601307.us.archive.org/8/items/short_science_fiction_collection_054_1602_librivox/ssf054_1000aplate_mckenty_mja_128kb.mp3",
+      "https://ia800609.us.archive.org/21/items/breakingtheouterring_2502_librivox/breakingtheouterring_01_chapin_128kb.mp3",
     thumbnail_url:
-      "https://m.media-amazon.com/images/I/71E8Hd4Ks4L._SY466_.jpg",
+      "https://m.media-amazon.com/images/I/81WlYSKzEGL._SY425_.jpg",
     genre: ["Fantastic Fiction", "Science Fiction", "Short Stories"],
+    download:
+      "https://ia800609.us.archive.org/21/items/breakingtheouterring_2502_librivox/breakingtheouterring_01_chapin_128kb.mp3",
   },
   {
     id: "2",
-    title: "The Adventures of Ulysses Cpt-1",
-    author: "Charles Lamb",
+    title: "Robinson Crusoe",
+    author: "Daniel Defoe",
     audio_url:
-      "https://ia804504.us.archive.org/3/items/ulysses_lamb_rd_librivox/ulysses_01_lamb.mp3",
+      "https://ia803406.us.archive.org/15/items/robinson_crusoe_1006_librivox/robinsoncrusoe_01_defoe.mp3",
     thumbnail_url:
-      "https://m.media-amazon.com/images/I/71JPaQ9vG3L._SY466_.jpg",
+      "https://m.media-amazon.com/images/I/91-QODvbm2L._SY466_.jpg",
     genre: ["Action", "Fiction", "Adventure"],
+    download:
+      "https://ia803406.us.archive.org/15/items/robinson_crusoe_1006_librivox/robinsoncrusoe_01_defoe.mp3",
   },
   {
     id: "3",
-    title: "Alice's Adventures in Wonderland",
-    author: "Lewis Carroll",
+    title: "The Adventures of Sherlock Holmes (version 4)",
+    author: "Sir Arthur Conan Doyle",
     audio_url:
-      "https://ia804601.us.archive.org/11/items/alices_adventures/aliceinwonderland_01_carroll.mp3",
-    thumbnail_url:
-      "https://m.media-amazon.com/images/I/91xhGWejJCL._SY425_.jpg",
+      "https://ia802300.us.archive.org/8/items/adventuressherlockholmes_v4_1501_librivox/adventuresofsherlockholmes_01_doyle_128kb.mp3",
+    thumbnail_url: "https://m.media-amazon.com/images/I/51+6dJxwl3L.jpg",
     genre: ["Fiction"],
+    download:
+      "https://ia802300.us.archive.org/8/items/adventuressherlockholmes_v4_1501_librivox/adventuresofsherlockholmes_01_doyle_128kb.mp3",
   },
   {
     id: "4",
-    title: "Left To Die",
-    author: "Blake Pierce",
+    title: "The Falcon on the Baltic",
+    author: "Edward Frederick Knight",
     audio_url:
-      "https://media.audiobooksnow.com/index.php?page=download_web&amp;action=playsample&amp;data=OTc4MTA5NDMwMjg5ODIwMjUwNTA3",
+      "https://dn721906.ca.archive.org/0/items/falcon_2011_librivox/falcon_01_knight_128kb.mp3",
     thumbnail_url:
-      "https://static.audiobooksnow.com/jackets/large/8/9/8/9781094302898.jpg",
+      "https://m.media-amazon.com/images/I/71xrDsmvbUL._SY466_.jpg",
     genre: ["Fiction", "Mystery"],
+    download:
+      "https://dn721906.ca.archive.org/0/items/falcon_2011_librivox/falcon_01_knight_128kb.mp3",
   },
   {
     id: "5",
-    title: "Already Gone",
-    author: "Bram Stoker",
+    title: "The Return of Sherlock Holmes",
+    author: "Sir Arthur Conan Doyle",
     audio_url:
-      "https://media.audiobooksnow.com/index.php?page=download_web&action=playsample&data=OTc4MTA5NDMxNjUzNjIwMjUwNTA3",
+      "https://ia802204.us.archive.org/16/items/return_holmes_0708_librivox/returnofholmes_01_doyle.mp3",
     thumbnail_url:
-      "	https://static.audiobooksnow.com/jackets/large/5/3/6/9781094316536.jpg",
+      "https://m.media-amazon.com/images/I/91hFhJIj4wL._SY445_.jpg",
     genre: ["Fiction", "Suspense"],
+    download:
+      "https://ia802204.us.archive.org/16/items/return_holmes_0708_librivox/returnofholmes_01_doyle.mp3",
   },
   {
     id: "6",
-    title: "The Tumor",
-    author: "John Grisham",
+    title: "The Hardy Boys: The Secret of the Caves",
+    author: "Franklin W. Dixon",
     audio_url:
-      "https://media.audiobooksnow.com/index.php?page=download_web&action=playsample&data=OTc4MTk4NzEwMzg5MjIwMjUwNTA3",
+      "https://ia600702.us.archive.org/3/items/secretofthecaves_2502_librivox/secretofthecaves_02_dixon_128kb.mp3",
     thumbnail_url:
-      "https://static.audiobooksnow.com/jackets/large/8/9/2/9781987103892.jpg",
+      "https://m.media-amazon.com/images/I/71MgYmLOMFL._AC_UY327_FMwebp_QL65_.jpg",
     genre: ["Nonfiction"],
+    download:
+      "https://ia600702.us.archive.org/3/items/secretofthecaves_2502_librivox/secretofthecaves_02_dixon_128kb.mp3",
   },
   {
     id: "7",
-    title: "Any Means Necessary",
-    author: "Jack Mars",
+    title: "Tales of Terror and Mystery",
+    author: "Sir Arthur Conan Doyle",
     audio_url:
-      "https://media.audiobooksnow.com/index.php?page=download_web&action=playsample&data=OTc4MTA5NDMxNDMwMzIwMjUwNTA3",
+      "https://ia803404.us.archive.org/6/items/terror_mystery_0707_librivox/talesofterror_01_doyle.mp3",
     thumbnail_url:
-      "https://static.audiobooksnow.com/jackets/large/3/0/3/9781094314303.jpg",
+      "https://ia803404.us.archive.org/6/items/terror_mystery_0707_librivox/Tales_of_Terror_and_Mystery_1003.jpg?cnt=0",
     genre: ["Fiction", "Suspense"],
+    download:
+      "https://ia803404.us.archive.org/6/items/terror_mystery_0707_librivox/talesofterror_01_doyle.mp3",
   },
   {
     id: "8",
-    title: "The Picture of Dorian Gray",
-    author: "Oscar Wilde",
+    title: "Kidnapped",
+    author: "Robert Louis Stevenson",
     audio_url:
-      "https://ia801501.us.archive.org/19/items/lostwarship_2504_librivox/lostwarship_06_williams_128kb.mp3",
-    thumbnail_url: "https://m.media-amazon.com/images/I/51IPJiX34fL.jpg",
+      "https://ia800606.us.archive.org/34/items/kidnapped_0807_librivox/kidnapped_01_stevenson.mp3",
+    thumbnail_url:
+      "https://ia600606.us.archive.org/34/items/kidnapped_0807_librivox/Kidnapped_1003.jpg?cnt=0",
     genre: ["Horror"],
+    download:
+      "https://ia800606.us.archive.org/34/items/kidnapped_0807_librivox/kidnapped_01_stevenson.mp3",
   },
   {
     id: "9",
-    title: "The Picture of Dorian Gray",
-    author: "Oscar Wilde",
+    title: "The Shore Road Mystery",
+    author: "Franklin W. Dixon",
     audio_url:
-      "https://ia801501.us.archive.org/19/items/lostwarship_2504_librivox/lostwarship_07_williams_128kb.mp3",
-    thumbnail_url: "https://m.media-amazon.com/images/I/51IPJiX34fL.jpg",
+      "https://dn721900.ca.archive.org/0/items/shoreroadmystery_2401_librivox/shorerroadmystery_01_dixon_128kb.mp3",
+    thumbnail_url:
+      "https://archive.org/download/shoreroadmystery_2401_librivox/shoreroad_2401.jpg",
     genre: ["Fiction", "Suspense"],
+    download:
+      "https://dn721900.ca.archive.org/0/items/shoreroadmystery_2401_librivox/shorerroadmystery_01_dixon_128kb.mp3",
   },
 ];
