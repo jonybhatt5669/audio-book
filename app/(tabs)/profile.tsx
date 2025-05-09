75cec15e1713f4b80badad18585123c7b311d12c
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-05-09 20:16:06 +0600

    Remove profile screen

--- app/(tabs)/profile.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-export default function Profile() {
-  return (
-    <SafeAreaView className="flex-1">
-      <Text>Profile</Text>
-    </SafeAreaView>
-  );
-}
